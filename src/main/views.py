4b78a183d75da6e892d96ada9d583ea851cbc4c7
Vinay Singh <vinaysingh02640@gmail.com>
2024-03-04 16:57:44 +0530

    something done more

--- src/main/views.py
+++ src/main/views.py
@@ -4,3 +4,6 @@ from django.http import HttpResponse
 # Create your views here.
 def main_view(request):
     return render(request, "views/main.html",{'name': "Automax"})
+
+def home_view(request):
+    return render(request, "views/home.html")
