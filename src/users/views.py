aeb8bbc99503021fbbc6807af394505155862d70
Vinay Singh <vinaysingh02640@gmail.com>
2024-03-18 17:09:44 +0530

    Logout Implemented

--- src/users/views.py
+++ src/users/views.py
@@ -2,8 +2,10 @@ from django.http import HttpResponse
 from django.shortcuts import render,redirect
 from django.contrib import messages
 from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from django.views import View
 
-from django.contrib.auth import authenticate, login
 # Create your views here.
 
 def login_view(request):
@@ -26,6 +28,27 @@ def login_view(request):
     return render(request, 'views/login.html', {'login_form': login_form})
 
 
-def register_view(request):
-    register_form = UserCreationForm()
-    return render(request, 'views/register.html', {'register_form': register_form})
+@login_required
+def logout_view(request):
+    logout(request)
+    return redirect('main')
+
+
+class RegisterView(View):
+
+    def get(self, request):
+        register_form = UserCreationForm()
+        return render(request, 'views/register.html', {'register_form': register_form})
+
+    def post(self, request):
+        register_form = UserCreationForm(request.POST)
+        if register_form.is_valid():
+            user = register_form.save()
+            user.refresh_from_db()
+            login(request, user)
+            messages.success(
+                request, f'User {user.username} registered successfully.')
+            return redirect('home')
+        else:
+            messages.error(request, f'An error occured trying to register.')
+            return render(request, 'views/register.html', {'register_form': register_form})
