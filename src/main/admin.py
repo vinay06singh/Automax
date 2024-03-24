e85d200557b37051e8d2ca5044c0a5712d72ea59
Vinay Singh <vinaysingh02640@gmail.com>
2024-03-24 09:13:57 +0530

    Car listing field added

--- src/main/admin.py
+++ src/main/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Listing
+
+
+class ListingAdmin(admin.ModelAdmin):
+    readonly_fields = ('id', )
+
+
+
+admin.site.register(Listing, ListingAdmin)
